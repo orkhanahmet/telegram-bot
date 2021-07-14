e2cfcab15827639489aec7acacbb26e3a0e0cfe6
orkhanahmet <35050948+orkhanahmet@users.noreply.github.com>
2021-07-14 14:53:03 +0400

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,6 @@
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, ConversationHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import os
 PORT = int(os.environ.get('PORT', 5000))
 
